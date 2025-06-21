1f9c5a4ed800b1c178701bf5fbb5c89b4f8896c9
my4kish <arlan.amirov.2015@mail.ru>
2025-06-21 20:46:32 +0500

    feat: finished website PC layout

--- frontend/src/app/components/main/second-page/second-page.component.ts
+++ frontend/src/app/components/main/second-page/second-page.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { NgScrollbarModule } from 'ngx-scrollbar';
+import { FormsModule } from '@angular/forms';
+import { CarouselModule } from 'ngx-owl-carousel-o';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-second-page',
-  imports: [CommonModule, NgScrollbarModule],
+  imports: [CommonModule, NgScrollbarModule, FormsModule, CarouselModule],
   templateUrl: './second-page.component.html',
   styleUrl: './second-page.component.css',
 })
@@ -36,4 +39,69 @@ export class SecondPageComponent {
       };
     }
   }
+
+  selected = '';
+  selectedContactMethod = '';
+  myNumber = '';
+
+  getPhone(): string {
+    return `${this.selected}${this.myNumber}`;
+  }
+
+  countries = [
+    { name: 'Казахстан', dialCode: '+7', flag: '🇰🇿' },
+    { name: 'Россия', dialCode: '+7', flag: '🇷🇺' },
+    { name: 'США', dialCode: '+1', flag: '🇺🇸' },
+    { name: 'Узбекистан', dialCode: '+998', flag: '🇺🇿' },
+    { name: 'Киргизия', dialCode: '+996', flag: '🇰🇬' },
+    { name: 'Турция', dialCode: '+90', flag: '🇹🇷' },
+  ];
+
+  contactMethods = [
+    { name: 'Telegram', value: 'telegram' },
+    { name: 'Whatsapp', value: 'whatsapp' },
+    { name: 'Звонок', value: 'callI' },
+  ];
+
+  onSubmit() {}
+
+    team = [
+    {
+      name: 'Руслан',
+      img: '/images/Ruslan.png',
+      position: 'SEO / Project Manager',
+      skills: ['Yandex Business','Wordstat','Google Trends','My Business'],
+      label: '4/5'
+    },
+    {
+      name: 'Ольга',
+      img: '/images/Olga.png',
+      position: 'UX/UI - designer',
+      skills: ['Figma','Landing Design','UX/UI Design','Web Design'],
+      label: '4/5'
+    },
+    {
+      name: 'Абылай',
+      img: '/images/Abylai.png',
+      position: 'Backend Developer',
+      skills: ['Python','FastAPI','Docker','SQL','Dart'],
+      label: '4/5'
+    }
+  ];
+
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    navText: ['< Назад','Далее >'],
+    nav: true,
+    responsive: {
+      0: { items: 1 },
+      740: { items: 2 },
+      1024: { items: 3 }
+    }
+  };
 }
